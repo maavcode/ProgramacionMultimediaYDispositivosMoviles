4d3fc8f2c3ec614107686133aeaa4ba3287a48a3
maavcode <maavcode@hotmail.com>
2025-10-01 12:24:59 +0200

    Ejercicio 2 en progreso

--- pdm.tema2/pdmtema2ejercicio2/app/src/main/java/com/example/pdmtema2ejercicio2/MainActivity.kt
+++ pdm.tema2/pdmtema2ejercicio2/app/src/main/java/com/example/pdmtema2ejercicio2/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pdmtema2ejercicio2.ui.theme.Pdmtema2ejercicio2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +36,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pdmtema2ejercicio2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    App(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
                     )
                 }
             }
@@ -31,17 +49,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App (modifier: Modifier = Modifier){
+    Parte1(modifier = modifier)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Pdmtema2ejercicio2Theme {
-        Greeting("Android")
+fun Parte1(modifier: Modifier = Modifier){
+    var texto by remember { mutableStateOf("0") }
+    Column {
+        Text(
+            text = "Calcular precio con IVA",
+            modifier = Modifier.padding(start = 16.dp, top = 30.dp)
+        )
+        OutlinedTextField(
+            value = texto,
+            label = {Text(text = stringResource(R.string.precio_del_producto))},
+            onValueChange = {texto=it},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Red, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(start = 16.dp)
+        )
     }
+}
+
+@Composable
+fun Parte2(){
+
 }
