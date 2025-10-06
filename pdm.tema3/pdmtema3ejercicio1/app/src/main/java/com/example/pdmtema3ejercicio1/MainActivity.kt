b8989156a1e9126108471df04bedf9420cd15e72
maavcode <maavcode@hotmail.com>
2025-10-06 14:35:28 +0200

    empezar ejercicio1 tema3

---
+++ pdm.tema3/pdmtema3ejercicio1/app/src/main/java/com/example/pdmtema3ejercicio1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pdmtema3ejercicio1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pdmtema3ejercicio1.ui.theme.Pdmtema3ejercicio1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pdmtema3ejercicio1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pdmtema3ejercicio1Theme {
+        Greeting("Android")
+    }
+}
