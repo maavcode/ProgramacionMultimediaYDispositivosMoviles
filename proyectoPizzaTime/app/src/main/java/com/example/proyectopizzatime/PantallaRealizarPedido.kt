b8614bf4fa2d0ba8f2ac1a96404afb74154bbf66
maavcode <maavcode@hotmail.com>
2025-10-16 00:14:24 +0200

    proceso

--- proyectoPizzaTime/app/src/main/java/com/example/proyectopizzatime/PantallaRealizarPedido.kt
+++ proyectoPizzaTime/app/src/main/java/com/example/proyectopizzatime/PantallaRealizarPedido.kt
@@ -1,18 +1,13 @@
 package com.example.proyectopizzatime
 
-import android.R.attr.text
-import androidx.annotation.ColorInt
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -30,12 +25,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.proyectopizzatime.ui.theme.ProyectoPizzaTimeTheme
-import java.nio.file.WatchEvent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun PantallaRealizarPedido(
     modifier: Modifier = Modifier
 ){
+    var tipoPizza by remember { mutableStateOf("") }
     LazyColumn (
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
@@ -57,24 +60,25 @@ fun PantallaRealizarPedido(
         }
 
         item {
-            Row (
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-            ){
-                var tipoPizza by remember { mutableStateOf("") }
+                    //.height(IntrinsicSize.Min) // para que la altura se ajuste al contenido
+            ) {
+
 
                 // Columna Izquierda
-                Column(Modifier
-                    .selectableGroup()
-                    .weight(1f)
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .selectableGroup()
                 ) {
                     Text(
                         text = "Pizza",
                         fontSize = 20.sp
                     )
+
                     val radioOptions = listOf("Romana", "Barbacoa", "Margarita")
-
-
                     radioOptions.forEach { radioOption ->
                         Row(
                             verticalAlignment = Alignment.CenterVertically,
@@ -90,31 +94,141 @@ fun PantallaRealizarPedido(
                             )
                         }
                     }
+
+                    // Imagen según tipoPizza
+
+
+
                 }
 
                 // Columna Derecha
-                Column (
-                    modifier = Modifier.weight(1f)
-                ){
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+
+                ) {
                     if (tipoPizza.isNotEmpty()){
+                        Column (
+
+                        ){
+                            Text(
+                                text = "Opciones de pizza",
+                                fontSize = 20.sp
+                            )
+                            OpcionesPizza(tipoPizza = tipoPizza)
+                        }
+
+
+                    }
+                }
+            }
+            if (tipoPizza.isNotEmpty()){
+                Row (
+                    modifier = Modifier.height(200.dp)
+                ){
+                    val imageOption = when(tipoPizza) {
+                        "Romana" -> R.drawable.person
+                        "Barbacoa" -> R.drawable.person
+                        "Margarita" -> R.drawable.person
+                        else -> R.drawable.person
+                    }
+
+                        Column (
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight(),
+                        ){
+                            Image(
+                                painter = painterResource(imageOption),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                        Column (
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+
+                        ){
+                            Text(
+                                text = "Cantidad"
+                            )
+                            Row (
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(10.dp)
+                            ){
+                                var cantidad by remember { mutableStateOf(0) }
+                                if (cantidad!=0){
+                                    Button(
+                                        onClick = {cantidad--},
+                                    ) {
+                                        Text("-")
+                                    }
+                                }
+
+
+                                Text(
+                                    text = cantidad.toString()
+                                )
+
+                                Button(
+                                    onClick = {cantidad++},
+                                ) {
+                                    Text("+")
+                                }
+                            }
+                        }
+
+
+
+
+                }
+            }
+            HorizontalDivider(
+                color = Color.Gray,
+                thickness = 1.dp
+            )
+
+
+        }
+
+        item {
+            var bebidaSeleccionada by remember { mutableStateOf("") }
+            
+            Row (
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                val listaBebidas = listOf("Agua", "Refresco", "Fanta")
+
+                listaBebidas.forEach { bebida ->
+                    Button(
+                        onClick = {bebidaSeleccionada=bebida},
+                        colors = ButtonDefaults
+                            .buttonColors(
+                                containerColor = if (bebidaSeleccionada == bebida) Color(0xFF4CAF50) else Color.Gray
+                            )
+                    ){
                         Text(
-                            text = "Opciones de pizza",
-                            fontSize = 20.sp
-                        )
-                        OpcionesPizza(
-                            tipoPizza = tipoPizza
+                            text = bebida
                         )
                     }
                 }
             }
-
-
 
             HorizontalDivider(
                 color = Color.Gray,
                 thickness = 1.dp
             )
         }
+
 
     }
 }
@@ -125,40 +239,53 @@ fun OpcionesPizza(
     tipoPizza: String
 ) {
     // Segun el tipo de pizza de doy unas opciones u otras
-    var radioOptions: List<String> = when (tipoPizza) {
+    var radioOptions: List<String> = emptyList()
+    when (tipoPizza) {
         "Romana" -> {
-            listOf("Con champiñones", "Sin champiñones")
+            radioOptions = listOf("Con champiñones", "Sin champiñones")
         }
 
         "Barbacoa" -> {
-           listOf("Carne de cerdo", "Carne de pollo", "Carne de ternera")
+            radioOptions = listOf("Carne de cerdo", "Carne de pollo", "Carne de ternera")
         }
 
         "Margarita" -> {
-           listOf("Con piña", "Sin piña", "vegana")
+            radioOptions = listOf("Con piña", "Sin piña", "vegana")
         }
-
-        else -> emptyList()
     }
     // Hago que la opcion que se vaya a elejir sea mutable
     var opcionPizza by remember { mutableStateOf("") }
-    Column {
-        radioOptions.forEach { radioOption ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(vertical = 4.dp)
-            ) {
-                RadioButton(
-                    selected = (radioOption == opcionPizza),
-                    onClick = { opcionPizza = radioOption }
-                )
-                Text(
-                    text = radioOption,
-                    modifier = Modifier.padding(start = 10.dp)
-                )
-            }
+
+        Column (
+
+        ){
+
+
+                radioOptions.forEach { radioOption ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(vertical = 4.dp),
+                    ) {
+                        RadioButton(
+                            selected = (radioOption == opcionPizza),
+                            onClick = { opcionPizza = radioOption }
+                        )
+                        Text(
+                            text = radioOption,
+                            modifier = Modifier.padding(start = 10.dp)
+                        )
+                    }
+                }
+
+
+
+
+
+
         }
-    }
+
+
 }
 
 @Preview(showBackground = true)
